b6013f9932bb3117d3e7553eb8f603a3548d569f
oceangala <oceangala@mail.ru>
2023-01-26 22:23:44 +0300

    initial commit with a code file

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿int lenLim = 3; // макс длина искомых элементов 
+
+string[] FillArray(int size)
+{
+    string[] stArray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter array element №{i + 1}: ");
+        stArray[i] = Console.ReadLine();
+    }
+    return stArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    { Console.Write($"{array[i]}   "); }
+    Console.WriteLine();
+}
+
+int GetSize()
+{
+    Console.Write("Enter array size: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    if (size < 1)
+    {
+        Console.WriteLine("Array size must be greater than 0, try again");
+        return GetSize();
+    }
+    else return size;
+}
+
+string[] FindElements(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= lenLim) count++;
+    }
+
+    if (count > 0)
+    {
+        string[] resArray = new string[count];
+        int i = 0;
+        while (i < count)
+        {
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j].Length <= 3)
+                {
+                    resArray[i] = array[j];
+                    i++;
+                }
+            }
+        }
+        return resArray;
+    }
+    else
+    {
+        string[] resArray = new string[1];
+        resArray[0] = "";
+        return resArray;
+    }
+}
+
+string[] startArray = FillArray(GetSize());
+Console.WriteLine();
+Console.WriteLine("Your start array is:");
+PrintArray(startArray);
+string[] res = FindElements(startArray);
+Console.WriteLine();
+Console.WriteLine("Your result array is:");
+PrintArray(res);
